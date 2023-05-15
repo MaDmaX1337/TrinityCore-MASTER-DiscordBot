ddd15db46dfb68f837abd0c9af013b0078a0057b
Maximilian Unsinn <masterbaron@web.de>
2023-05-15 16:58:54 +0200

    Initial Release

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+        token: "",
+        prefix: "!",
+        statusMessage: "with my 60 characters!",
+        color: "#0000FF",
+
+        databaseHost: 'localhost',
+        databaseUser     : 'root',
+        databasePassword : 'root',
+
+        soapPort: 7878,
+        soapHostname: "127.0.0.1",
+        soapAuth: "soapName:soapPassword"
+
+}
